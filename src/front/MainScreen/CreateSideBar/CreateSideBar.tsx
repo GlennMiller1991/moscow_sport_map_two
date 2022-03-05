80358ea6456697eaad7a8a647c975aeecb680245
GlennMiller1991 <gatesoftommorow91@mail.ru>
2022-03-05 12:11:49 +0300

    CreateSideBar logic

--- src/front/MainScreen/CreateSideBar/CreateSideBar.tsx
+++ src/front/MainScreen/CreateSideBar/CreateSideBar.tsx
@@ -6,13 +6,43 @@ import fitnessGirl from "../Sidebar/fitnessGirl.png";
 import Line from "../Sidebar/Line.png";
 import {CreateField} from "./CreateField/CreateField";
 import {CreateSelect} from "./CreateSelect/CreateSelect";
+import {useFormik} from "formik";
+
+type formType = {
+    lat: null | number,
+    lng: null | number,
+    name: string,
+    address: string,
+}
+
+type formErrorType = {
+    lat?: boolean,
+    lng?: boolean,
+    name?: boolean,
+    address?: boolean,
+}
 
 export const CreateSideBar: React.FC = () => {
-
     const [sideBarVisibility, setSideBarVisibility] = useState(true)
-    const onSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-    }
+
+
+    const formData = useFormik({
+        initialValues: {
+            lat: null,
+            lng: null,
+            name: '',
+            address: '',
+        } as formType,
+        validate: (values) => {
+            const errors: formErrorType = {}
+            if (!values.lat) {
+
+            }
+        },
+        onSubmit: (values: formType) => {
+            formData.resetForm()
+        }
+    })
 
     //callbacks
     const toggleSideBar = useCallback(() => {
@@ -22,7 +52,7 @@ export const CreateSideBar: React.FC = () => {
     return (
         <div className={sideBarVisibility ? `${styles.sidebar}` : `${styles.sidebar} ${styles.sidebarHidden}`}>
             <div className={styles.wrapper}>
-                <form onSubmit={onSubmitHandler}>
+                <form onSubmit={formData.handleSubmit}>
                     <CreateField text={'Широта'}
                                  keyName={'latitude'}
                                  placeholder={'Введите значение'}/>
@@ -32,21 +62,24 @@ export const CreateSideBar: React.FC = () => {
                     <CreateField text={'Название спортивного объекта'}
                                  keyName={'name'}
                                  placeholder={'Введите название объекта'}/>
-                    <CreateField text={'Ведомственная принадлежность'}
-                                 keyName={'org'}
-                                 placeholder={'Введите название ведомтсва'}/>
-                    <CreateField text={'Наименование спортивной зоны'}
-                                 keyName={'sportzone'}
-                                 placeholder={'Введите название спортивной зоны'}/>
-                    <CreateSelect text={'Тип спортивной зоны'}
-                                  keyName={'zonetypeId'}
-                                  mocks={spr_zonetype}/>
-                    <CreateSelect text={'Вид спорта'}
-                                  keyName={'sportId'}
-                                  mocks={spr_sport}/>
-                    <CreateSelect text={'Доступность'}
-                                  keyName={'affinityId'}
-                                  mocks={spr_affinity}/>
+                    <CreateField text={'Адрес'}
+                                 keyName={'address'}
+                                 placeholder={'Введите адрес объекта'}/>
+                    {/*<CreateField text={'Ведомственная принадлежность'}*/}
+                    {/*             keyName={'org'}*/}
+                    {/*             placeholder={'Введите название ведомтсва'}/>*/}
+                    {/*<CreateField text={'Наименование спортивной зоны'}*/}
+                    {/*             keyName={'sportzone'}*/}
+                    {/*             placeholder={'Введите название спортивной зоны'}/>*/}
+                    {/*<CreateSelect text={'Тип спортивной зоны'}*/}
+                    {/*              keyName={'zonetypeId'}*/}
+                    {/*              mocks={spr_zonetype}/>*/}
+                    {/*<CreateSelect text={'Вид спорта'}*/}
+                    {/*              keyName={'sportId'}*/}
+                    {/*              mocks={spr_sport}/>*/}
+                    {/*<CreateSelect text={'Доступность'}*/}
+                    {/*              keyName={'affinityId'}*/}
+                    {/*              mocks={spr_affinity}/>*/}
                     <div className={styles.mix}>
                         <button type={'submit'} className={styles.submit}>
                             Добавить объект
